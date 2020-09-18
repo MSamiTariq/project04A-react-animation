0dde34172e600e316795d4abf5b28db5e3b4dbb9
samitariq12 <samitariq40@gmail.com>
2020-09-18 20:54:01 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import spiderman from './images/spiderman.gif';
+import useWebAnimations from '@wellyshen/use-web-animations';
+
+
 
 function App() {
+  const { ref, getAnimation, animate } = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(-100%,0)' },
+      { transform: 'translate(520%,0)' }
+    ],
+    timing: {
+      duration: 5000,
+      playbackRate: 1,
+      iterations: Infinity,
+      easing: "ease-in-out",
+      direction: 'reverse'
+    }
+  });
+
+  const play = () => {
+    getAnimation().play();
+  }
+  const pause = () => {
+    getAnimation().pause();
+  };
+  const reverse = () => {
+    getAnimation().reverse();
+  };
+  const goSlow = () => {
+    getAnimation().updatePlaybackRate(getAnimation().playbackRate * 0.9);
+  };
+
+  const goFaster = () => {
+    const anim = getAnimation();
+    anim.updatePlaybackRate(anim.playbackRate * 1.1);
+  };
+  const [playBackRate, setPlayBackRate] = useState(1);
+
+  const updatePlayBackRate = (val) => { setPlayBackRate(val) }
+
+  useEffect(() => {
+    document.getElementById("fast").addEventListener("click", (e) => {
+        updatePlayBackRate(getAnimation().playbackRate);
+    });
+    document.getElementById("slow").addEventListener("click", (e) => {
+        updatePlayBackRate(getAnimation().playbackRate);
+    });
+}, [animate]);
+
+    function mouseEnter(e) {
+      e.target.style.background = '#b07563';
+      e.target.style.color = 'white';
+    }
+
+    function mouseLeave(e) {
+      e.target.style.background = 'white';
+      e.target.style.color = 'black';
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={spiderman} className="img" ref={ref} alt="cartoon" />
+      <div className = "but">
+        <button className = "button" onClick = {play}  onMouseEnter={(e) => mouseEnter(e)}
+        onMouseLeave={(e) => mouseLeave(e)}>Play</button>
+        <button className = "button" onClick = {pause}  onMouseEnter={(e) => mouseEnter(e)}
+        onMouseLeave={(e) => mouseLeave(e)}>Pause</button>
+        <button className = "button" id = "fast" onClick = {goFaster}  onMouseEnter={(e) => mouseEnter(e)}
+        onMouseLeave={(e) => mouseLeave(e)}>Speed Up</button>
+        <button className = "button" id = "slow" onClick = {goSlow}  onMouseEnter={(e) => mouseEnter(e)}
+        onMouseLeave={(e) => mouseLeave(e)}>Speed Down</button>
+        <button className = "button" onClick = {reverse}  onMouseEnter={(e) => mouseEnter(e)}
+        onMouseLeave={(e) => mouseLeave(e)}>Reverse</button>
+      </div>
+      <div className = "meter_div">
+        <label htmlFor="meter" ><b className = "text">SPEED: </b></label>
+        <meter className = "meter" id="meter" min="0" max="10" low="0" high="6" optimum="2" value={playBackRate}>
+          at 50/100
+        </meter>
+      </div>
     </div>
   );
 }
